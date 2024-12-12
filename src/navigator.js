a67bbe831d2e18a2c553c1b917e6180283839801
Angelo <ripamontiangelo@itis-molinari.eu>
2024-12-12 12:49:03 +0100

    3 pagine prese da cipiaceinfo.it

---
+++ src/navigator.js
@@ -0,0 +1,24 @@
+const hide = (elements) => {
+    elements.forEach((element) => {
+        element.classList.add("hidden");
+    });
+}
+
+const show = (element) => {
+    element.classList.remove("hidden");
+}
+
+export const createNavigator = (parentElement) => {
+    const pages = Array.from(parentElement.querySelectorAll(".ripamontiPageSPA"));
+
+    const render = () => {
+        const url = new URL(document.location.href);
+        const pageName = url.hash.replace("#", "");
+        const selected = pages.filter((page) => page.id === pageName)[0] || pages[0];
+
+        hide(pages);
+        show(selected);
+    }
+    window.addEventListener('popstate', render);
+    render();
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "meida",
+    darkMode: "media",
     content: [
+      "./index.html",
       "./src/**/*.{html,js}",
     ],
     theme: {
